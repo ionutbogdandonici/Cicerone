f6b87c3ebfa02eb05c94a68cd62707fea6cf1adc
Bogdan Donici <83654129+ionutbogdandonici@users.noreply.github.com>
2021-12-01 14:14:02 +0100

    Commento

--- app/src/main/java/Cicerone/I_Tappa.java
+++ app/src/main/java/Cicerone/I_Tappa.java
@@ -1,7 +1,10 @@
 package Cicerone;
 
+/**
+ *  Interfaccia che rappresenta una tappa, essa è caratterizzata da un nome...
+ *
+ *
+ */
 public interface I_Tappa {
-
     String getName();
-    String nappo();
-    }
+}
