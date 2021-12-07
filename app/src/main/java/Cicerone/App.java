f8630d872ba1a7070aaa161a8675ea873c5aa78e
ionutbogdandonici <83654129+ionutbogdandonici@users.noreply.github.com>
2021-12-07 16:59:36 +0100

    Updates

--- app/src/main/java/Cicerone/App.java
+++ app/src/main/java/Cicerone/App.java
@@ -2,9 +2,12 @@ package Cicerone;
 
 import Cicerone.db.DB_Controller;
 
+import java.sql.SQLException;
+
 public class App {
 
-    public static void main(String[] args){
-        DB_Controller.init();
+    public static void main(String[] args) throws SQLException {
+        String query = "SELECT * FROM tag";
+        DB_Controller.query(query);
     }
 }
--- app/src/main/java/Cicerone/db/DB_Controller.java
+++ app/src/main/java/Cicerone/db/DB_Controller.java
@@ -1,26 +1,46 @@
 package Cicerone.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DB_Controller {
 
     private static final String url = "jdbc:mysql://localhost:3306/cicerone";
     private static final String username = "root";
     private static final String password = "";
+    private static Connection con;
 
-    public static void init(){
+    public static boolean initTest() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            return false;
         }
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connected!");
+            return true;
         } catch (SQLException e) {
             throw new IllegalStateException("Not connected", e);
+        }
+    }
+
+    public static void query(String query) throws SQLException {
+        if (initTest()) {
+            con = DriverManager.getConnection(url, username, password);
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            System.out.println("ID | Nome");
+            System.out.println("----------------");
+            while(rs.next()){
+                //Display values
+                System.out.print(rs.getString("ID_TAG"));
+                System.out.print("  | ");
+                System.out.print(rs.getString("Nome"));
+                System.out.print("\n");
+            }
+        } else {
+            throw new IllegalStateException("Impossibile eseguire query, DB non accessibile!");
         }
     }
 }
