574c25a882912edd0388134851edb2eff5741abe
ionutbogdandonici <83654129+ionutbogdandonici@users.noreply.github.com>
2021-12-07 16:11:08 +0100

    Updates

---
+++ app/src/main/java/Cicerone/App.java
@@ -0,0 +1,10 @@
+package Cicerone;
+
+import Cicerone.db.DB_Controller;
+
+public class App {
+
+    public static void main(String[] args){
+        DB_Controller.init();
+    }
+}
---
+++ app/src/main/java/Cicerone/db/DB_Controller.java
@@ -0,0 +1,20 @@
+package Cicerone.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_Controller {
+
+    private static final String url = "jdbc:mysql://localhost:3306/cicerone";
+    private static final String username = "root";
+    private static final String password = "";
+
+    public static void init(){
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Not connected", e);
+        }
+    }
+}
