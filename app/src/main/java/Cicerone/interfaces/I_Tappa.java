b33d8b845ffb07176bd3785c90d086e03eb6b3b5
Davide <davide.nappo@studenti.unicam.it>
2021-12-08 19:46:59 +0100

    implementazione I_Tappa

--- app/src/main/java/Cicerone/interfaces/I_Tappa.java
+++ app/src/main/java/Cicerone/interfaces/I_Tappa.java
@@ -1,10 +1,76 @@
 package Cicerone.interfaces;
 
 /**
- *  Interfaccia che rappresenta una tappa, essa è caratterizzata da un nome...
- *
+ *  Interfaccia che rappresenta una tappa, essa è caratterizzata da un id,un nome,una descrizione,
+ *  una stringa che definisce come raggiungere la tappa (String raggiunta),un'Area e un territorio.
  *
  */
 public interface I_Tappa {
+
+    /**
+     * Restituisce l'id della tappa
+     * @return id percorso
+     */
+    int getId();
+
+    /**
+     * Ritorna il nome della tappa
+     * @return nome
+     */
     String getName();
+
+    /**
+     * Inserisce il nome per la tappa
+     * @param nome
+     */
+    void setNome(String nome);
+
+    /**
+     * Restituisce la descrizione della tappa
+     * @return descrizione tappa
+     */
+    String getDescrizione();
+
+    /**
+     * Permette di inserire una descrizione per la tappa
+     * @param descrizione
+     */
+    void setDescrizione(String descrizione);
+
+    /**
+     * Restituisce dettagli su come avvengono gli spostamenti verso(?) questa tappa
+     * @return raggiunta
+     */
+    String getRaggiunta();
+
+    /**
+     * Permette di inserire dettagli su come avviene spostamento verso(?) questa tappa
+     * @param raggiunta
+     */
+    void setRaggiunta(String raggiunta);
+
+    /**
+     * Ritorna  l'area della tappa
+     * @return area
+     */
+    I_Area getArea();
+
+    /**
+     * Permette di inserire l'area della tappa
+     * @param area
+     */
+    void setArea(I_Area area);
+
+    /**
+     * Ritorna il territorio della tappa
+     * @return territorio
+     */
+    I_Territorio getTerritorio();
+
+    /**
+     * Permette di inserire un territorio
+     * @param territorio
+     */
+    void setTerritorio(I_Territorio territorio);
+
 }
