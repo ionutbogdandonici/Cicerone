aee87368dfcb6a2415b4f4a0e2ab39080fee158f
Riccardo <riccardo.ceccarani@studenti.unicam.it>
2021-12-20 17:13:37 +0100

    Implementazione classe Area e modifica interfaccia I_Area

--- app/src/main/java/Cicerone/controllers/ControllerGestisciArea.java
+++ app/src/main/java/Cicerone/controllers/ControllerGestisciArea.java
@@ -1,4 +1,68 @@
 package Cicerone.controllers;
 
-public class ControllerGestisciArea {
+import Cicerone.classes.Area;
+import Cicerone.classes.Territorio;
+import Cicerone.db.DB_Controller;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ControllerGestisciArea implements I_ControllerGestisciArea {
+
+    private Territorio territorioArea;
+    private Set<Area> aree = new HashSet<>();
+    ControllerGestisciTerritorio controllerGestisciTerritorio = new ControllerGestisciTerritorio();
+
+    @Override
+    public boolean insertInDB(String toponoimo, String NomeTerritorio) throws SQLException {
+        String ID_Territorio = controllerGestisciTerritorio.getByName(NomeTerritorio).getID();
+        //TODO CONTROLLO ERRORI
+        if (checkDB(toponoimo, ID_Territorio)) {
+            DB_Controller.insertQuery("INSERT INTO area (Toponimo, ID_TERRITORIO) VALUES ('" + toponoimo + "', '" + ID_Territorio + "')");
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void getAllData() throws SQLException {
+        ResultSet resultSet = DB_Controller.selectAllFromTable("area");
+        while (resultSet.next()) {
+            aree.add(new Area(
+                    resultSet.getString("ID_AREA"),
+                    resultSet.getString("Toponimo"),
+                    resultSet.getString("ID_TERRITORIO")));
+        }
+    }
+
+    @Override
+    public Area getById(String ID_Area) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Area getByToponimo(String Toponimo) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Set<Area> getAllByTerritorio(String ID_Territorio) {
+        return null;
+    }
+
+    @Override
+    public Territorio getTerritorioByToponimo(String toponimo) {
+        return null;
+    }
+
+
+    private boolean checkDB(String toponimo, String ID_TERRITORIO) throws SQLException {
+        String query = "SELECT ID_AREA, Toponimo, ID_TERRITORIO FROM area WHERE Toponimo =\"" + toponimo + "\" AND ID_TERRITORIO=\"" + ID_TERRITORIO + "\"";
+        System.out.println(DB_Controller.getNumberRows(query));
+        return DB_Controller.getNumberRows(query) == 0;
+    }
+
+
 }
---
+++ app/src/main/java/Cicerone/controllers/I_ControllerGestisciArea.java
@@ -0,0 +1,24 @@
+package Cicerone.controllers;
+
+import Cicerone.classes.Area;
+import Cicerone.classes.Territorio;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+public interface I_ControllerGestisciArea {
+
+    boolean insertInDB(String Toponoimo, String NomeTerritorio) throws SQLException;
+
+    void getAllData() throws SQLException;
+
+    Area getById(String ID_Area) throws SQLException;
+
+    Area getByToponimo(String Toponimo) throws SQLException;
+
+    Set<Area> getAllByTerritorio(String ID_Territorio);
+
+    Territorio getTerritorioByToponimo(String toponimo);
+
+
+}
