b40cfc50615a469d420ec513d079d9c6df144276
Bogdan <ionutbogdan.donici@studenti.unicam.it>
2021-12-20 15:48:56 +0100

    Updates

--- app/src/main/java/Cicerone/interfaces/I_Area.java
+++ app/src/main/java/Cicerone/interfaces/I_Area.java
@@ -4,14 +4,10 @@ package Cicerone.interfaces;
  * Interfaccia per rappresentare un'area
  */
 public interface I_Area {
-    
+
     String getID();
 
     String getToponimo(String ID_Area);
-
-    boolean addToponimo(String toponimo);
-
-    boolean setToponimo(String ID_Area, String newToponimo);
 
     int getTerritorio(String toponimo);
 }
