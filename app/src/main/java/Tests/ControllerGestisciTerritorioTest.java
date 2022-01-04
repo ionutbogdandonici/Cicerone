ae4a3ebd62c2fdaa3168152a68089630d216eb5a
Bogdan <ionutbogdan.donici@studenti.unicam.it>
2022-01-04 18:26:22 +0100

    Intervento Davide Nappo

---
+++ app/src/main/java/Tests/ControllerGestisciTerritorioTest.java
@@ -0,0 +1,28 @@
+package Tests;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ControllerGestisciTerritorioTest {
+
+    @Test
+    void insertInDB() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
